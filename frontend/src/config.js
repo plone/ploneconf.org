f9c75c7eb727a3c554f47e8957d8deef65e54146
Victor Fernandez de Alba <sneridagh@gmail.com>
2021-09-22 19:54:11 +0200

    Talks, first version

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -14,7 +14,7 @@
  */
 
 import { customBlocks } from '@package/components/Blocks';
-import { Person } from '@package/components';
+import { Person, Talk } from '@package/components';
 import DefaultTeaserBody from '@kitconcept/volto-blocks-grid/components/Teaser/DefaultBody';
 import {
   PersonsListingBody,
@@ -48,6 +48,7 @@ export default function applyConfig(config) {
   };
 
   config.views.contentTypesViews.Person = Person;
+  config.views.contentTypesViews.Talk = Talk;
   config.blocks.blocksConfig.teaser.variations = [
     {
       id: 'default',
