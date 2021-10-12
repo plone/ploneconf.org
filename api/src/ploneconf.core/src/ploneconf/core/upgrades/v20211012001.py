14a1a1b6668afa1204ea33546878b2f274ad06f1
Érico Andrei <ericof@gmail.com>
2021-10-12 18:24:04 +0200

    Fix lint.

--- api/src/ploneconf.core/src/ploneconf/core/upgrades/v20211012001.py
+++ api/src/ploneconf.core/src/ploneconf/core/upgrades/v20211012001.py
@@ -8,10 +8,10 @@ def fix_tz(setup_tool=None):
     logger.info("Running upgrade (Python): 20211012001")
     portal = api.portal.get()
 
-    results = api.content.find(portal, portal_type=['Talk', 'Keynote'])
+    results = api.content.find(portal, portal_type=["Talk", "Keynote"])
     for brain in results:
         talk = brain.getObject()
-        talk.start = talk.start.replace(tzinfo=timezone('Europe/Berlin'))
-        talk.end = talk.end.replace(tzinfo=timezone('Europe/Berlin'))
+        talk.start = talk.start.replace(tzinfo=timezone("Europe/Berlin"))
+        talk.end = talk.end.replace(tzinfo=timezone("Europe/Berlin"))
         talk.reindexObject()
         logger.info(f"-- Reindexed {talk.title}")
