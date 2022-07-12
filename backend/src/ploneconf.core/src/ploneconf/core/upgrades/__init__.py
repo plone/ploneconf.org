3b6f0ba851713bc145095180704cfffd8dbe1c5a
Érico Andrei <ericof@gmail.com>
2022-07-12 10:24:33 -0300

    Upgrade Plone to version 6.0.0a6

---
+++ backend/src/ploneconf.core/src/ploneconf/core/upgrades/__init__.py
@@ -0,0 +1,8 @@
+from plone import api
+
+
+def upgrade_plone(context):
+    """Upgrade Plone to latest version."""
+    mt = api.portal.get_tool("portal_migration")
+    if mt.needUpgrading():
+        mt.upgrade()
