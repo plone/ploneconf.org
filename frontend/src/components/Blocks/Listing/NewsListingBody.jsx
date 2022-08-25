ca79239f77978abfd00f5e73b7786cdd1dc96020
jimbiscuit <julienchandelle@gmail.com>
2022-08-25 15:17:44 +0200

    Add button to news page on listing news

--- frontend/src/components/Blocks/Listing/NewsListingBody.jsx
+++ frontend/src/components/Blocks/Listing/NewsListingBody.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { injectIntl, FormattedMessage } from 'react-intl';
-import { Image } from 'semantic-ui-react';
+import { Image, Icon } from 'semantic-ui-react';
 import { ConditionalLink } from '@plone/volto/components';
 
 const NewsListingBody = ({ items, linkTitle, linkHref, isEditMode }) => {
+
+  const newsLink = "/news";
+
   return (
     <div className="news-list">
       <div className="wrapper-aside text">
         <h2 className="aside-title">
-          <FormattedMessage id="News" defaultMessage="News" />
+          <a href={newsLink}>
+            <FormattedMessage id="News" defaultMessage="News" />
+          </a>
         </h2>
       </div>
       <div className="news-listing">
@@ -33,6 +38,10 @@ const NewsListingBody = ({ items, linkTitle, linkHref, isEditMode }) => {
           );
         })}
       </div>
+      <a href="/news" className="news-more">
+        <Icon name="add" />
+        <FormattedMessage id="More News" defaultMessage="More News" />
+      </a>
     </div>
   );
 };
